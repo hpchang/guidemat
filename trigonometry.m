4160a418fe8654eb3e25a2b5f7f675532989a823
hpchang <hpchang.tw@gmail.com>
2016-05-18 17:46:17 +0800

    plot sine wave via slider

--- trigonometry.m
+++ trigonometry.m
@@ -135,6 +135,9 @@ function slider_value_Callback(hObject, eventdata, handles)
 % hObject    handle to slider_value (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+NewVal = get(hObject,'Value');
+NewVal=round(NewVal);
+set(handles.edit_value, 'string', NewVal);
 
 % Hints: get(hObject,'Value') returns position of slider
 %        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
